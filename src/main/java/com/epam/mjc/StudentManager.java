f05213b27f7aef1d08364a2244eddaa6913c48dd
martam00 <marta.michniacka1@gmail.com>
2023-07-26 19:42:02 +0200

    custom exception

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -6,7 +6,7 @@ public class StudentManager {
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) throws StudentNotFoundException{
-    if (studentID < 11) {
+    if (studentID < IDs.length) {
       return Student.getValueOf(studentID);
     } else {
       throw new StudentNotFoundException("Could not find student with ID " + studentID);
